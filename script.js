4fefb14837482024d9b919576b9d0852fd97cea0
lmorgan2398 <lmorgan2398@gmail.com>
2025-03-09 12:04:23 -0500

    Finish gameboard module
    Gameboard module stores basic methods for adding to the stored
    gameboard and evaluating for a win or draw

--- script.js
+++ script.js
@@ -13,21 +13,35 @@ const gameboard = (function() {
         };
     };
 
-    const winConditions = [
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
 
     const checkWinConditions = (marker) => {
+        const winConditions = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
         for(let i = 0; i < 8; i++){
-            let winCondition = winConditions[i];
-            
+            let [a, b, c] = winConditions[i];
+            if(board[a] === marker && board[b] === marker && board[c] === marker) {
+                return true;
+            }
         }
-    }
+        return false;
+    };
+
+    const checkForDraw = () => {
+        if(!board.some(cell => cell === '')) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    return { getBoard, resetBoard, placeMarker, checkWinConditions, checkForDraw }
 })();
